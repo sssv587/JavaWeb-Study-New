28eea156207feb00a351cba2463c18d69403cc20
sssv587 <1092663295@qq.com>
2024-03-21 23:45:59 +0800

    Servlet-注解方式配置Servlet

---
+++ 06-Servlet/src/main/java/com/futurebytedance/servlet/Servlet2.java
@@ -0,0 +1,24 @@
+package com.futurebytedance.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+/**
+ * @author yuhang.sun
+ * @version 1.0
+ * @date 2024/3/21 - 23:39
+ * @Description
+ */
+//@WebServlet("/xxx")
+@WebServlet(urlPatterns = {"/xxx", "xxx1"})
+public class Servlet2 extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("执行啦...");
+    }
+}
