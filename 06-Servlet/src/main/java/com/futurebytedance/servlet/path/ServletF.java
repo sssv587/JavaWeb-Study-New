a143cac789939583fbd37213f28fbe6606b8ebf7
sssv587 <1092663295@qq.com>
2024-04-02 01:05:32 +0800

    Servlet-前端相对路径问题

---
+++ 06-Servlet/src/main/java/com/futurebytedance/servlet/path/ServletF.java
@@ -0,0 +1,23 @@
+package com.futurebytedance.servlet.path;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+/**
+ * @author yuhang.sun
+ * @version 1.0
+ * @date 2024/4/2 - 0:33
+ * @Description
+ */
+@WebServlet("/servletF")
+public class ServletF extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.service(req, resp);
+    }
+}
