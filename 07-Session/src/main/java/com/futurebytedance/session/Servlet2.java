90f36fed695d47a68d3577a7e536b687e2d7389e
sssv587 <1092663295@qq.com>
2024-04-12 00:04:55 +0800

    会话管理-Session

--- 07-Session/src/main/java/com/futurebytedance/session/Servlet2.java
+++ 07-Session/src/main/java/com/futurebytedance/session/Servlet2.java
@@ -17,13 +17,14 @@ import java.util.Arrays;
 public class Servlet2 extends HttpServlet {
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        // 获得session对象
         Cookie[] cookies = req.getCookies();
         if (null != cookies) {
             for (Cookie cookie : cookies) {
                 System.out.println(cookie.getName() + "=" + cookie.getValue());
             }
         }
+
+        // 获得session对象
         HttpSession session = req.getSession();
         System.out.println(session.getId());
         System.out.println(session.isNew());
