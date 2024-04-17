0c93f84b85d6bcb263691a295b8948725102062d
sssv587 <1092663295@qq.com>
2024-04-18 00:04:48 +0800

    Ajax-JS原生XMLHTTPRequest方式实现

---
+++ 07-Session/src/main/java/com/futurebytedance/ajax/HelloServlet.java
@@ -0,0 +1,28 @@
+package com.futurebytedance.ajax;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+/**
+ * @author yuhang.sun
+ * @version 1.0
+ * @date 2024/4/17 - 23:55
+ * @Description Ajax
+ * js代码在index.html
+ */
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // 接收参数
+        String username = req.getParameter("username");
+
+        // 做出响应
+        resp.getWriter().write("hello" + username);
+    }
+}
