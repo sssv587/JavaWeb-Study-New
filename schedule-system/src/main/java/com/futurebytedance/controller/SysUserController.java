72104c9e56e1abd47b630e933e5c8e444c48eb84
sssv587 <1092663295@qq.com>
2024-05-25 17:29:52 +0800

    日程管理第六期-注册功能完成

--- schedule-system/src/main/java/com/futurebytedance/controller/SysUserController.java
+++ schedule-system/src/main/java/com/futurebytedance/controller/SysUserController.java
@@ -14,6 +14,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 
 /**
@@ -50,19 +51,17 @@ public class SysUserController extends BaseController {
      * 接收用户注册请求的业务处理方法(业务接口 不是java中的interface)
      */
     protected void regist(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        // 1、接收客户端提交的参数
-        String username = req.getParameter("username");
-        String userPwd = req.getParameter("userPwd");
+        // 1、接收客户端提交的Json参数,并转换为User对象,获取信息
+        SysUser registUser = WebUtil.readJson(req, SysUser.class);
+
         // 2、调用服务层方法,完成注册功能
-        // 将参数放入一个SysUser对象中,再调用regist方法时传入
-        SysUser sysUser = new SysUser(null, username, userPwd);
-        int rows = userService.regist(sysUser);
+        int rows = userService.regist(registUser);
         // 3、根据注册结果(成功、失败) 做页面跳转
-        if (rows > 0) {
-            resp.sendRedirect("/registSuccess.html");
-        } else {
-            resp.sendRedirect("/registFail.html");
+        Result result = Result.ok(null);
+        if (rows < 1) {
+            result = Result.build(null, ResultCodeEnum.USERNAME_USED);
         }
+        WebUtil.writeJson(resp, result);
     }
 
     /**
